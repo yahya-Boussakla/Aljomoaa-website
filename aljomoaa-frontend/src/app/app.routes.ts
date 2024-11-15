40ac349d1a678fc21579de8cd1b3eb81620475c2
yahya <yahyaboussakla@gmail.com>
2024-11-15 22:47:15 +0100

    initialite home and header component and modules

--- aljomoaa-frontend/src/app/app.routes.ts
+++ aljomoaa-frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HeaderComponent,
+    },
+];
